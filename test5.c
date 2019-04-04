323d94640882e82b58226e069c4b05a1a0b9cdf7
wake5up <48407764+wake5up@users.noreply.github.com>
2019-04-04 22:43:45 +0800

    Update test5.c

--- test5.c
+++ test5.c
@@ -61,3 +61,52 @@ int main()
 	system("pause");
 	return 0;
 }
+
+
+public class GradeExam 
+{
+    public static void main(String[] args) 
+    {
+        char[][] answers = {
+            {'A','B','A','C','C','D','E','E','A','D'},
+            {'D','B','A','B','C','A','E','E','A','D'},
+            {'E','D','D','A','C','B','E','E','A','D'},
+            {'C','B','A','E','D','C','E','E','A','D'},
+            {'A','B','D','C','C','D','E','E','A','D'},
+            {'B','B','E','C','C','D','E','E','A','D'},
+            {'B','B','A','C','C','D','E','E','A','D'},
+            {'E','B','E','C','C','D','E','E','A','D'},
+            };
+        char[] keys = {'D','B','D','C','C','D','A','E','A','D'};
+        int k=0;
+        int[] a=new int [8];
+        for(int i=0;i<answers.length;i++)
+        {
+            int j=0;
+            for(j=0;j<answers[i].length;j++)
+            {
+                if(answers[i][j]==keys[j])
+                //correctCount++;
+                 a[i]++;
+            }
+        }
+        int[] b=new int [a.length];
+        int i=0;
+        int j=0;
+        for(i=0;i<a.length;i++)
+        {
+            b[i]=a[i];
+        }
+        java.util.Arrays.sort(a);
+        for(i=a.length-1;i>=0;i--)
+            for(j=0;j<a.length;j++)
+            {
+                if(a[i]==b[j])
+                {
+                    System.out.println("Student"+j+"'s correct count is"+a[i]);
+                    break;
+                }
+            }
+    }
+    
+}
