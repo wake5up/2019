e1eac6e70750ea90fd48540b14a4a5b95060e8eb
郝团团 <2740276135@qq.com>
2019-10-03 22:42:39 +0800

    练习排序代码

--- _sort/源.c
+++ _sort/源.c
@@ -2,7 +2,7 @@
 
 
 #include<stdio.h>
-
+#define Q_INSERT 8
 void insertsort(int* arr, int len)
 {
 	int i;
@@ -57,6 +57,98 @@ void BubbleSort(int *arr, int len)
 		k += 1;
 	}
 }
+
+void QSort(int *arr, int start,int end)
+{
+	int i, j;
+	int flag = 1;
+	if (start > end)
+	{
+		return;
+	}
+	i = start;
+	j = end;
+	while (i<j)
+	{
+		if (arr[i] > arr[j])
+		{
+			Swap(&arr[i],&arr[j]);
+			flag = !flag;
+		}
+		flag ? j-- : i++;
+	}
+	QSort(arr,start,i-1);
+	QSort(arr, i + 1, end);
+}
+
+int Partition(int *arr, int start, int end)//三数取中法
+{
+	int mid = (end - start) >> 1 + start;
+	if (arr[start] > arr[mid])
+	{
+		Swap(arr + start, arr + mid);
+	}
+	if (arr[mid] > arr[end])
+	{
+		Swap(arr+mid,arr+end);
+	}
+	if (arr[start] > arr[mid])
+	{
+		Swap(arr + start, arr + mid);
+	}
+	Swap(arr+mid,arr+end-1);
+
+	int i = start + 1;
+	int j = end - 2;
+	while (i < j)
+	{
+		while (arr[i] < arr[end - 1])
+		{
+			i++;
+		}
+		if (i!=j)
+		{
+			while (arr[j]>arr[end - 1])
+			{
+				j--;
+			}
+			Swap(arr + i, arr + j);
+		}
+		
+	}
+	Swap(arr + i, arr + end - 1);
+	printf("%d\n", i);
+	return i;
+	
+}
+
+void HQSort(int *arr, int start, int end)
+{
+	int midValue = Partition(arr, start, end);
+	if (end - start > Q_INSERT)
+	{
+		QSort(arr, start, midValue - 1);
+		QSort(arr, midValue + 1, end);
+	}
+	else
+	{
+		insertsort(arr, end - start + 1);
+	}
+}
+
+void dealmergesort(int *arr, int *temp, int start, int end)
+{
+	int mid = (end - start) >> 1 + start;
+	int i = 0;
+	int j = mid+1;
+	for ()
+}
+void mergesort(int *arr, int len)
+{
+	int *temp = (int *)malloc(sizeof(int)*len);
+	dealmergesort(arr,temp,0,len-1);
+	free(temp);
+}
 void PrintfFun(int *arr, int len)
 {
 	int i;
@@ -69,10 +161,12 @@ void PrintfFun(int *arr, int len)
 int main()
 {
 	int i;
-	int arr[] = { 1, 2, 5, 3, 8, 6, 0, 5 };
+	int arr[] = { 1, 2, 5, 3, 8, 6, 0, 5,9,7 };
 	//  insertsort(arr,sizeof(arr)/sizeof(arr[0]));
 	//ShellSort(arr, sizeof(arr) / sizeof(arr[0]));
-	BubbleSort(arr, sizeof(arr) / sizeof(arr[0]));
+	//BubbleSort(arr, sizeof(arr) / sizeof(arr[0]));
+	//QSort(arr,0,sizeof(arr)/sizeof(arr[0])-1);
+	//HQSort(arr, 0,sizeof(arr) / sizeof(arr[0])-1);
 	PrintfFun(arr, sizeof(arr) / sizeof(sizeof(arr[0])));
 	return 0;
 }
