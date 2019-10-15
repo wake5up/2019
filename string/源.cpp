f80e6f62976dee6cd1c393f3e60aa937f09083ee
郝团团 <2740276135@qq.com>
2019-10-15 22:06:43 +0800

    练习string类的拷贝构造

--- string/源.cpp
+++ string/源.cpp
@@ -55,6 +55,7 @@ int main()
 }
 #endif
 
+#if 0
 //浅拷贝
 namespace ht
 {
@@ -91,7 +92,9 @@ namespace ht
 		}
 	};
 }
+#endif
 
+#if 0
 //传统深拷贝
 namespace ht
 {
@@ -106,7 +109,7 @@ namespace ht
 			{
 				str = "";
 			}
-			str = new char[strlen(str) + 1];
+			_str = new char[strlen(str) + 1];
 			strcpy(_str,str);
 		}
 		string(const string& s)
@@ -136,3 +139,247 @@ namespace ht
 	};
 }
 
+#endif
+namespace ht
+{
+	class string
+	{
+	private:
+		char* _str;
+	public:
+		string(char* str="")
+		{
+			if (str == nullptr)
+			{
+				str = "";
+			}
+			_str = new char[strlen(str) + 1];
+			strcpy(_str,str);
+		}
+		string(const string& s)
+			:_str(nullptr)
+		{
+			string temp(s._str);
+			swap(_str,temp._str);
+		}
+		/*string& operator=(const string& s)
+		{
+			if (this != &s)
+			{
+				string temp = s._str;
+				swap(_str,temp._str);
+			}
+			return *this;
+		}*/
+		string& operator=(string s)
+		{
+			swap(_str,s._str);
+			return *this;
+		}
+		~string()
+		{
+			if (_str)
+			{
+				delete[] _str;
+				_str = nullptr;
+			}
+		}
+	};
+}
+
+//namespace bite
+//{
+//	class string
+//	{
+//	public:
+//		string(char* str = "")
+//		{
+//			if (nullptr == str)
+//				str = "";
+//
+//			// 申请空间
+//			_str = new char[strlen(str) + 1];
+//			_count = 1;
+//			// 存放str中的字符
+//			strcpy(_str, str);
+//		}
+//
+//		string(string& s)
+//			: _str(s._str)
+//			, _count(++s._count)
+//		{}
+//
+//		// s2 = s1;
+//		string& operator=(const string& s)
+//		{
+//			if (this != &s)
+//			{
+//				// ...
+//			}
+//
+//			return *this;
+//		}
+//
+//		~string()
+//		{
+//			if (_str && 0 == --_count)
+//			{
+//				delete[] _str;
+//				_str = nullptr;
+//			}
+//		}
+//	private:
+//		char* _str;
+//		int _count;
+//	};
+//}
+
+//namespace bite
+//{
+//	class string
+//	{
+//	public:
+//		string(char* str = "")
+//		{
+//			if (nullptr == str)
+//				str = "";
+//
+//			// 申请空间
+//			_str = new char[strlen(str) + 1];
+//			_count = 1;
+//			// 存放str中的字符
+//			strcpy(_str, str);
+//		}
+//
+//		string(const string& s)
+//			: _str(s._str)
+//		{
+//			++_count;
+//		}
+//
+//		// s2 = s1;
+//		string& operator=(const string& s)
+//		{
+//			if (this != &s)
+//			{
+//				// ...
+//			}
+//
+//			return *this;
+//		}
+//
+//		~string()
+//		{
+//			if (_str && 0 == --_count)
+//			{
+//				delete[] _str;
+//				_str = nullptr;
+//			}
+//		}
+//	private:
+//		char* _str;
+//		static int _count;
+//	};
+//
+//	int string::_count = 0;
+//}
+
+#if 0
+namespace ht
+{
+	class string
+	{
+	private :
+		char* _str;
+		int* _count;
+	public :
+		string(char* str="")
+			: _count(new int(1))
+			, _str(new char[strlen(str) + 1])
+		{
+			if (str == nullptr)
+			{
+				str = "";
+			}
+			
+			strcpy(_str,str);
+		}
+		string(const string& s)
+			:_str(s._str)
+			, _count(s._count)
+		{
+			++(*_count);
+		}
+		string operator=(string& s)
+		{
+			if (this != &s)
+			{
+				if (0 == --(*_count))
+				{
+					delete[] _str;
+					delete _count;
+					_str = nullptr;
+					_count = nullptr;
+				}
+				_str = s._str;
+				_count = s._count;
+				++(*_count);
+			}
+			return *this;
+		}
+		~string()
+		{
+			if (_str&&0 == --(*_count))
+			{
+				delete[] _str;
+				delete _count;
+				_str = nullptr;
+				_count = nullptr;
+			}
+		}
+		char& operator[](size_t index)
+		{
+			if ((*_count) > 1)
+			{
+				string temp(_str);
+				this->swap(temp);
+			}
+			return _str[index];
+		}
+		void swap(string& s)
+		{
+			std::swap(_str,s._str);
+			std::swap(_count,s._count);
+		}
+		const char& operator[](size_t index)const
+		{
+			return _str[index];
+		}
+	};
+}
+#endif
+
+void TestString()
+{
+	ht::string s1("hello");
+	ht::string s2(s1);
+	ht::string s3("wrold");
+	ht::string s4(s3);
+
+	// s3不需要释放原来的资源，因为还有s4在用
+	s3 = s1;
+
+	// s4是最后使用资源的对象， 赋值期间必须释放原来的资源
+	s4 = s1;
+
+	/*s1[0] = 'H';
+	char& rc = s1[1];
+	rc = 'E';*/
+}
+
+
+int main()
+{
+	TestString();
+	return 0;
+}
